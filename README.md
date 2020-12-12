1b28e684699d6f00c72dc30d25741e4db0b34b18
GMkonan <guilhemont@hotmail.com>
2020-12-12 21:53:25 -0200

    added codeblock highlight syntax in dracula

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # MyBlog
 A blog I made so I can post with markdown! Check Others websites I post. Dev.to
 ## Description
-A blog I made with tailwindcss and nextJS so I can post Articles with markdown.
+A blog I made with bootstrap react and nextJS so I can post Articles with markdown.
 
  ### Authors
  - [GMkonan](https://github.com/GMkonan)
--- pages/[blog].js
+++ pages/[blog].js
@@ -1,6 +1,17 @@
 import matter from 'gray-matter';
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism';
+
+const CodeBlock = ({ language, value }) => {
+
+    return (
+        <SyntaxHighlighter showLineNumbers={true} language={language} style={dracula}>
+            {value}
+        </SyntaxHighlighter>
+    );
+};
 
 const Blog = ({ content, data }) => {
     const frontmatter = data;
@@ -8,7 +19,7 @@ const Blog = ({ content, data }) => {
     <div>
         <h1>{frontmatter.title}</h1>
         <h3>{frontmatter.description}</h3>
-        <ReactMarkdown escapeHtml={true} source={content} />
+        <ReactMarkdown escapeHtml={true} source={content} renderers={{ code: CodeBlock }} />
     </div>
     )
 };
