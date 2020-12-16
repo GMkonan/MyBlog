71a9eabd192f501b762335d33e679a4b8649f935
GMkonan <guilhemont@hotmail.com>
2020-12-16 07:34:06 -0200

    added bloglist file and refactored

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,9 @@
-import matter from 'gray-matter';
 import React from 'react';
 import Layout from '../components/Layout';
-import Link from "next/link";
 import Header from '../components/Header'
 import styled from 'styled-components';
-import Introduction from '../components/introduction';
-
-const Articles = styled.div`
-    margin: 0 auto;
-    width: 800px;
-    border-left-width: 0.1rem;
-    border-right-width: 0.1rem;
-    border-left-style: solid;
-    border-right-style: solid;
-    border-color: rgb(130, 141, 154, 0.4);
-`;
+import Introduction from '../components/Introduction';
+import BlogList from '../components/BlogList';
 
 const Title = styled.h1`
     margin-left: 10px;
@@ -30,30 +19,7 @@ const Space = styled.p`
     padding-bottom: 20px;
 `;
 
-
-const ArticTitle = styled.a`
-    font-size: 1.6rem;
-`;
-
-const List = styled.ul`
-    list-style-type:none;
-`;
-
-const Items = styled.li`
-    margin:0 0 20px 0;  
-`;
-
-const SpanDate = styled.span`
-    margin: 10px 0 0 0;
-    display: inline-block;
-`;
-
 const Home = ({title, description, data}) => {
-    //map the data going to each file and getting the parts
-    //between ---- with matter
-    const RealData = data.map((blog) => matter(blog));
-    //go to each of the "tags" that we got with matter and get data
-    const ListItems = RealData.map((listItem) => listItem.data);
 
     return (
         <div>
@@ -62,20 +28,7 @@ const Home = ({title, description, data}) => {
                 <Introduction />
                 <Title>{title}</Title>
                 <Space></Space>
-                <Articles>
-                
-                <List >
-                    {ListItems.map((blog, i) => (
-                        <Items key={i}>
-                            <Link href={`/${blog.slug}`}>
-                                <ArticTitle>{blog.title}</ArticTitle>
-                            </Link>
-                            <br />
-                            <SpanDate>{blog.date}</SpanDate>
-                        </Items>
-                    ))}
-                </List>
-                </Articles>
+                <BlogList data={data}/>
             </Layout>
             </div>
     )
