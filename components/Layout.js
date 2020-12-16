e30bd3ada679eb566a422d5a6631d25e4b2a21e8
GMkonan <guilhemont@hotmail.com>
2020-12-16 07:12:55 -0200

    removed netlify

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,3 @@
-import styled from 'styled-components';
 import GlobalStyle from '../styles/GlobalStyle';
 import Head from 'next/head';
 
@@ -6,9 +5,6 @@ const Layout = ({ children }) => {
 
     return (
         <div>
-                                <Head>
-          <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-        </Head>
             <GlobalStyle />
             <main>
                 <div>{children}</div>
