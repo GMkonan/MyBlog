2efc649b659f3277407e53c8e4f148fda5cfa780
GMkonan <guilhemont@hotmail.com>
2020-12-17 03:33:09 -0200

    resolving path intro

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Layout from '../components/Layout';
 import Header from '../components/Header'
 import styled from 'styled-components';
-import Introduction from '../components/Introduction';
+import Introduction from './../components/Introduction';
 import BlogList from '../components/BlogList';
 
 const Title = styled.h1`
